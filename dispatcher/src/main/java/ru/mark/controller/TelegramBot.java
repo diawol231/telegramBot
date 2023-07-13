c81d238c66f4f4d3ad946ffe431be1800a7480e1
Mark Forat <foratik2001@gmail.com>
2023-07-13 17:22:08 +0700

    Обновление

--- dispatcher/src/main/java/ru/mark/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/mark/controller/TelegramBot.java
@@ -1,7 +1,9 @@
 package ru.mark.controller;
 
+import javax.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.internal.inject.UpdaterException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -17,12 +19,18 @@ public class TelegramBot extends TelegramLongPollingBot {
   private String botName;
   @Value("${bot.token}")
   private String botToken;
-
+  private UpdateController updateController;
+  public TelegramBot(UpdateController updateController){
+    this.updateController = updateController;
+  }
+  @PostConstruct
+  public void init(){
+    updateController.registerBot(this);
+  }
   @Override
   public String getBotUsername() {
     return botName;
   }
-
   @Override
   public String getBotToken() {
     return botToken;
@@ -30,13 +38,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
   @Override
   public void onUpdateReceived(Update update) {
-    var originalMessage = update.getMessage();
-    log.debug(originalMessage.getText());
-
-    var response = new SendMessage();
-    response.setChatId(originalMessage.getChatId().toString());
-    response.setText("Hello from bot");
-    sendAnswerMessage(response);
+    updateController.processUpdate(update);
   }
 
   public void sendAnswerMessage(SendMessage message){

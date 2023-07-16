d188c19c1e420de4667a51a820cac1404adbbfc2
diawol231 <foratik2001@gmail.com>
2023-07-16 20:08:19 +0700

    создание реста

---
+++ rest-service/src/main/java/ru/mark/service/FileService.java
@@ -0,0 +1,13 @@
+package ru.mark.service;
+
+import org.springframework.core.io.FileSystemResource;
+import ru.mark.entity.AppDocument;
+import ru.mark.entity.AppPhoto;
+import ru.mark.entity.BinaryContent;
+
+public interface FileService {
+    AppDocument getDocument(String id);
+    AppPhoto getPhoto(String id);
+    FileSystemResource getFileSystemResource(BinaryContent binaryContent);
+
+}
---
+++ rest-service/src/main/java/ru/mark/service/impl/FileServiceImpl.java
@@ -0,0 +1,55 @@
+package ru.mark.service.impl;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+import ru.mark.dao.AppDocumentDAO;
+import ru.mark.dao.AppPhotoDAO;
+import ru.mark.entity.AppDocument;
+import ru.mark.entity.AppPhoto;
+import ru.mark.entity.BinaryContent;
+import ru.mark.service.FileService;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+@Log4j
+public class FileServiceImpl implements FileService {
+    private final AppDocumentDAO appDocumentDAO;
+    private final AppPhotoDAO appPhotoDAO;
+
+    public FileServiceImpl(AppDocumentDAO appDocumentDAO, AppPhotoDAO appPhotoDAO) {
+        this.appDocumentDAO = appDocumentDAO;
+        this.appPhotoDAO = appPhotoDAO;
+    }
+
+    @Override
+    public AppDocument getDocument(String docId) {
+        //TODO добавить дешефрование хеш-строки
+        var id = Long.parseLong(docId);
+        return appDocumentDAO.findById(id).orElse(null);
+    }
+
+    @Override
+    public AppPhoto getPhoto(String photoId) {
+        //TODO добавить дешефрование хеш-строки
+        var id = Long.parseLong(photoId);
+        return appPhotoDAO.findById(id).orElse(null);
+    }
+
+    @Override
+    public FileSystemResource getFileSystemResource(BinaryContent binaryContent) {
+        try {
+            //TODO добавить генерацию имени временного файла
+            File temp = File.createTempFile("tempFile", ".bin");
+            temp.deleteOnExit();
+            FileUtils.writeByteArrayToFile(temp, binaryContent.getFileAsArrayOfBytes());
+            return new FileSystemResource(temp);
+        } catch (IOException e) {
+            log.error(e);
+            return null;
+        }
+    }
+}
